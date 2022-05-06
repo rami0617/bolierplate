731fc03c21483dba1d30aa589bd042f89225ba38
rami <aarmakrr@hanmail.net>
2022-05-07 01:41:41 +0900

    feat: Make a schema

--- models/Users.js
+++ models/Users.js
@@ -17,5 +17,20 @@ const userSchema = mongoose.Schema({
   lastname: {
     type: String,
     maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
   }
-})
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
